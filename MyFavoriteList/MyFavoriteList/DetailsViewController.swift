3f1f3fbb6aaf0b56953b4982b16309de740505ed
CanerCagri <canerrcagrii@gmail.com>
2022-03-29 17:47:00 +0300

    User Interface created

---
+++ MyFavoriteList/MyFavoriteList/DetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailsViewController.swift
+//  MyFavoriteList
+//
+//  Created by Caner Çağrı on 29.03.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var priceTextField: UITextField!
+    @IBOutlet var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func saveBtnTapped(_ sender: Any) {
+    }
+    
+   
+
+}
---
+++ MyFavoriteList/MyFavoriteList/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyFavoriteList
+//
+//  Created by Caner Çağrı on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
