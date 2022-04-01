a3f6c30ead01e24ffa7ca042406b0bdf5e7ef99d
CanerCagri <canerrcagrii@gmail.com>
2022-04-01 09:53:40 +0300

    Coredata updated

--- MyFavoriteList/MyFavoriteList/DetailsViewController.swift
+++ MyFavoriteList/MyFavoriteList/DetailsViewController.swift
@@ -6,18 +6,88 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
-    
     @IBOutlet var priceTextField: UITextField!
+    @IBOutlet var saveButtpn: UIButton!
     @IBOutlet var nameTextField: UITextField!
+    
+    var selectedItemName = ""
+    var selectedItemUUID : UUID?
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if selectedItemName != "" {
+            // Show selected item options from CoreData
+            saveButtpn.isEnabled = false
+            
+            if let uuidString = selectedItemUUID?.uuidString {
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Favorite")
+                
+                // Adding filter    %@ mean = When uudString equal to id, filter data with id
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                do {
+                    let sonuclar = try context.fetch(fetchRequest)
+                    
+                    if sonuclar.count > 0 {
+                        for sonuc in sonuclar as! [NSManagedObject] {
+                            
+                            if let name = sonuc.value(forKey: "name") as? String {
+                                nameTextField.text = name
+                            }
+                            if let price = sonuc.value(forKey: "price") as? Int {
+                                priceTextField.text = String(price)
+                            }
+                            if let image = sonuc.value(forKey: "image") as? Data {
+                                let imageLoad = UIImage(data: image)
+                                imageView.image = imageLoad
+                            }
+                        }
+                    }
+                }catch {
+                    print("Error when loading data")
+                }
+                
+            }
+        } else {
+            saveButtpn.isEnabled = false
+            nameTextField.text = ""
+            priceTextField.text = ""
+           
+        }
+        
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
+        
+        
+        imageView.isUserInteractionEnabled = true
+        let imageGesture = UITapGestureRecognizer(target: self, action: #selector(pickImage))
+        imageView.addGestureRecognizer(imageGesture)
+    }
+    
+    @objc func pickImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        saveButtpn.isEnabled = true
+        self.dismiss(animated: true)
     }
     
     @objc func closeKeyboard() {
@@ -25,8 +95,27 @@ class DetailsViewController: UIViewController {
     }
     
     @IBAction func saveBtnTapped(_ sender: Any) {
-    }
-    
-   
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let context = (appDelegate?.persistentContainer.viewContext)!
+        let favorite = NSEntityDescription.insertNewObject(forEntityName: "Favorite", into: context)
 
+        favorite.setValue(nameTextField.text!, forKey: "name")
+        
+        if let price = Int(priceTextField.text!) {
+        favorite.setValue(price, forKey: "price")
+        }
+        favorite.setValue(UUID(), forKey: "id")
+        
+        let imageData = imageView.image!.jpegData(compressionQuality: 0.5)
+        favorite.setValue(imageData, forKey: "image")
+        
+        do {
+            try context.save()
+            print("Save completed!")
+        }catch {
+            print("Error , didnt save!")
+        }
+        NotificationCenter.default.post(name: NSNotification.Name("dataEntered"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+    }
 }
--- MyFavoriteList/MyFavoriteList/ViewController.swift
+++ MyFavoriteList/MyFavoriteList/ViewController.swift
@@ -6,21 +6,142 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var nameArray : [String] = []
+    var idArray : [UUID] = []
+    
+    var selectedName = ""
+    var selectedUUID : UUID?
+    
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(rightbarTapped))
+        
+        loadData()
+    }
+    
+    // Calling before screen loading in here we using for taking datas to Tableview
+    override func viewWillAppear(_ animated: Bool) {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(loadData), name: NSNotification.Name(rawValue: "dataEntered") , object: nil)
+    }
+    
+    @objc func loadData() {
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // Returning NSFectRequestResult
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Favorite")
+        
+        // When loading big data , we can use Caches with this code.
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            // if results not empty , load data
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String {
+                        nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        idArray.append(id)
+                    }
+                }
+                tableView.reloadData()
+            }
+            
+            
+        } catch {
+            print("Get error when loading data")
+        }
+        
     }
     
     @objc func rightbarTapped() {
+        selectedName = ""
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destination = segue.destination as! DetailsViewController
+            destination.selectedItemName = selectedName
+            destination.selectedItemUUID = selectedUUID
+        }
+    }
 
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedName = nameArray[indexPath.row]
+        selectedUUID = idArray[indexPath.row]
+        
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    // Removing section
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let uuidString = idArray[indexPath.row].uuidString
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Favorite")
+            fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == idArray[indexPath.row] {
+                                context.delete(result)
+                                
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                
+                                self.tableView.reloadData()
+                                
+                                do {
+                                    try context.save()
+                                }catch {
+                                    
+                                }
+                                
+                                break
+                            }
+                        }
+                    }
+                }
+            }catch {
+                print("Error when deleting")
+            }
+        }
+    }
 }
 
